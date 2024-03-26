5629669dd9908a27dbf28383ff1c25da2d7d0a3d
Snenhlanhl <133172353+Snenhlanhl@users.noreply.github.com>
2024-03-26 22:54:13 +0200

    added footer

--- Script.js
+++ Script.js
@@ -1,1 +1,28 @@
 alert ("YOU ARE THE BEST GEE!");
+
+let { GoolgeAPIClient } = require('gooleapis');                             // to include google library 
+
+let credentials = newGoogleAPIClient().credentaials();                      //to make request to YouTupe API
+
+function getToken() {
+    let credentials = newGoogleAPIClient().credentials();                  // to get access token  authorization server
+
+    //...
+}
+
+let authorizationUrl = credentials.getAuthorization 
+
+button.addEventListener("click",getToken);
+
+function getToken() {
+    let response = await
+    fetch(authorizationUrl);
+
+    response.then(function(response){
+        let credentails = respnse.json();
+        console.log(credentails);
+    });
+}
+credentails.getToken().then(function(acceccToken){
+    console.log(accessToken);
+});
