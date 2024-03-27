5ec2ef7feb75a788ca1be400297667ee80169ac0
Snenhlanhl <133172353+Snenhlanhl@users.noreply.github.com>
2024-03-27 03:02:45 +0200

    added JavaScript

--- Script.js
+++ Script.js
@@ -26,3 +26,73 @@ function getToken() {
 credentails.getToken().then(function(acceccToken){
     console.log(accessToken);
 });
+
+
+// let apiUrl = "https://www.googleapis.com/youtube/v3/";
+
+function search() {
+let apiKey ="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";
+let apiUrl =`https://www.googleapis.com/youtube/v3/search?key=${apikey}&part=snippet&q=query`;
+axios.get(apiUrl).then();
+}
+
+
+function getData() {
+    let response =awaitfetch(apiUrl);
+}
+if (response.ok === false) {
+    console.error("Error making request to YouTube API");
+}
+response.then(response => response.json());
+let data = response.data;
+
+let dataObj = {
+    title: data.title,
+    views:data.views,
+    likes: data.likes,
+    dislikes: data.dislikes,
+    likesDislikesRatio:(data.likes/dataDislikes)*100
+
+};
+
+function callback(dataObj) {
+    let dataObj = {
+        title: data.title,
+        views:data.views,
+        likes: data.likes,
+        dislikes: data.dislikes,
+        likesDislikesRatio:(data.likes/dataDislikes)*100
+    };
+
+}
+getData().then(callback);
+
+function renderData() {
+    let container = document.getElementById('dataContainer');
+    let dataObj = getData().then(callback);
+    container.innerHTML= JSON.string(dataObj);
+
+    container.innerHTML+="<h1>Title:</h1>";
+    container.innerHTML+="<p>Description:</p>";
+    container.innerHTML+="<p>Views:</p>";
+    container.innerHTML+="<P>Likes:</p>";
+    container.innerHTML+="<p>Dislikes:</P>";
+    container.innerHTML+="<P>Published on:</p>" + Data.parse(response.data.publishedAt)+ "GMT";
+    container.innerHTML+="<img src=" + response.data.thumbnailUrl + "alt=" + response.data.videold +"/>";
+
+}
+function submit(){
+    if (document.querySelector('input').value==="") {
+        return false;
+    }
+    document.querySelector('input').value=document.querySelector('input').value.trim(); 
+    try{
+        //code here 
+    }
+    catch (err) {
+        console.error(erro.message);
+        document.querySelector('input').value="";
+        return false;
+    }
+    
+}
